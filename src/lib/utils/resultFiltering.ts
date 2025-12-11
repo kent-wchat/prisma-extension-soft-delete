550aeeb703fe2897f34faa9bb124e6cb29f978d1
kent-wchat <kent@wonderchat.io>
2025-12-11 10:54:19 +0800

    modify result modifiers to account for multiple states for non-deleted

--- src/lib/utils/resultFiltering.ts
+++ src/lib/utils/resultFiltering.ts
@@ -6,20 +6,34 @@ export function shouldFilterDeletedFromReadResult(
   config: ModelConfig
 ): boolean {
   return (
+    // Filter if where not specified
     !params.args.where ||
+    // Filter if where specified but delete marker col not included
     typeof params.args.where[config.field] === "undefined" ||
-    !params.args.where[config.field]
+    // Filter if not specifying to include deleted documents
+    (config.isDeletedValueUnique
+      ? params.args.where[config.field] !== config.createValue(true)
+      : !params.args.where[config.field])
   );
 }
 
 export function filterSoftDeletedResults(result: any, config: ModelConfig) {
   // filter out deleted records from array results
   if (result && Array.isArray(result)) {
+    if (config.isDeletedValueUnique)
+      return result.filter(
+        (item) => item[config.field] !== config.createValue(true)
+      );
     return result.filter((item) => !item[config.field]);
   }
 
   // if the result is deleted return null
-  if (result && result[config.field]) {
+  if (
+    result &&
+    (config.isDeletedValueUnique
+      ? result[config.field] === config.createValue(true)
+      : result[config.field])
+  ) {
     return null;
   }
 
